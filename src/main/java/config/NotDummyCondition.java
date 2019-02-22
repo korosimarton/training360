7f74ba0a952b4366c4a559b9c103fa99040efb0c
korosi <marton.korosi@accenture.com>
2019-02-22 09:16:40 +0100

    DummyCondition.java, NotDummyCondition.java

---
+++ src/main/java/config/NotDummyCondition.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class NotDummyCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        if (!conditionContext.getEnvironment().containsProperty("mode") ||
+                !conditionContext.getEnvironment().getProperty("mode").equals("dummy")) {
+            return true;
+        }
+        return false;
+    }
+}
